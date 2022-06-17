fdfaa72b6178798ca5a3f2c44f2834513b532c2a
Kenny Winata <jimhwang678@gmail.com>
2022-06-17 16:11:44 +0700

    first commit

---
+++ lib/music_data/music_data.dart
@@ -0,0 +1,26 @@
+List <Map<String, String>> musicsQueue = [
+    {
+      "id": "1",
+      "name": "Heavenly Sword",
+      "mandarin": "何為永恆",
+      "imgUrl": "assets/images/heavenly_sword.jpg",
+      "author": "Hu Xia",
+      "musicUrl": "assets/musics/hewei.mp3"
+    },
+    {
+      "id": "2",
+      "name": "How Have You Been",
+      "mandarin": "你，好不好？",
+      "imgUrl": "assets/images/ni_haobuhao.jpg",
+      "author": "Eric Zhou",
+      "musicUrl": "assets/musics/nihaobuhao.mp3"
+    },
+    {
+      "id": "3",
+      "name": "Those Year's",
+      "mandarin": "那些年",
+      "imgUrl": "assets/images/naxienian.jpg",
+      "author": "Hu Xia",
+      "musicUrl": "assets/musics/naxienian.mp3"
+    },
+  ];
